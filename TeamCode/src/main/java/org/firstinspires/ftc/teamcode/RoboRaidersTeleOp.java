a2c11bbc53a5c38e59828b68608a80f744c0f89f
Alex <albobby@hvc.rr.com>
2017-12-18 14:59:46 -0500

    changed some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboRaidersTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboRaidersTeleOp.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 /**
- * Created by Katelin Zichittella on 12/18/2017.
+ * Created by Katelin Zichittella and Nick Urbin on 12/18/2017.
  */
 
 public abstract class RoboRaidersTeleOp extends OpMode {
