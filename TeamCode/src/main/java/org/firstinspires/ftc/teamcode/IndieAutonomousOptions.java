8ec2a4bb86c423fa7359090d073bfac65dffbcd6
jason1011 <jcsember@gmail.com>
2018-01-06 14:08:47 -0500

    Updates IndieautonomousOptions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IndieAutonomousOptions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IndieAutonomousOptions.java
@@ -75,8 +75,10 @@ public class IndieAutonomousOptions extends RoboRaidersAuto {
 
         String selectionsOk = new String("Selections Great :)");
 
+        selectedOptions[0][1] = "no";
+
         // Configure for Indie autonomous
-        while (!selectedOptions[0][1].equals("yes")) {
+        while (selectedOptions[0][1].equals("no")) {
             configForAuto(allianceSelPrompt, alliancePosResps, 1, selectedOptions);
             configForAuto(bsSelPrompt, bsPosResps, 2, selectedOptions);
             configForAuto(jewelSelPrompt, yesNoOptions, 3, selectedOptions);
