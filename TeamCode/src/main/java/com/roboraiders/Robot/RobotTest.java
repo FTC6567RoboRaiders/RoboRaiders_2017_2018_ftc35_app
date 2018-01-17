0ecc11cddcc8160a9dba5ff0626bb7cdb1cb204e
Alex <albobby@hvc.rr.com>
2018-01-17 15:32:22 -0500

    test

--- TeamCode/src/main/java/com/roboraiders/Robot/RobotTest.java
+++ TeamCode/src/main/java/com/roboraiders/Robot/RobotTest.java
@@ -38,8 +38,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 public class RobotTest {
 
 
-
-
     /* Robot Motors, Servos, CR Servos and Sensors */
     public DcMotor motorFrontLeft = null;
     public DcMotor motorFrontRight = null;
