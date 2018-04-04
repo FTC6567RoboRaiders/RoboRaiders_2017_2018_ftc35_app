695be47eca1e2d55b2de432023a1e0fb536a8c49
Nick <nicksurbin@frontier.com>
2018-04-04 14:53:55 -0400

    Added RelishAutonomous and RelishAutonomousHeader

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelishAutonomous.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by Nick Urbin on 4/4/2018.
+ */
+
+@Autonomous
+
+public class RelishAutonomous extends RelishAutonomousHeader {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize();
+
+        waitForStart();
+
+        // Start Code Here
+
+
+
+
+
+
+    }
+}
